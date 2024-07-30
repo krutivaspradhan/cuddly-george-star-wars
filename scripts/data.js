b885187bb0a0c2404bb170c791b5e533ecd623a4
Krutivas Pradhan <72146433+krutivaspradhan@users.noreply.github.com>
2024-07-30 12:04:46 -0500

    Create data.js

---
+++ scripts/data.js
@@ -0,0 +1,81 @@
+// data.js
+const moviesData = [
+    {
+      title: "Rogue One",
+      releaseDate: "2016",
+      director: "Gareth Edwards",
+      rating: 7.8,
+      description: "The Rebel Alliance makes a risky move to steal the plans for the Death Star."
+    },
+    {
+      title: "Revenge of the Sith",
+      releaseDate: "2005",
+      director: "George Lucas",
+      rating: 7.5,
+      description: "Three years into the Clone Wars, the Jedi rescue Palpatine from Count Dooku."
+    },
+    {
+      title: "A New Hope",
+      releaseDate: "1977",
+      director: "George Lucas",
+      rating: 8.6,
+      description: "Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a Wookiee, and two droids to save the galaxy."
+    },
+    {
+      title: "The Phantom Menace",
+      releaseDate: "1999",
+      director: "George Lucas",
+      rating: 6.5,
+      description: "Two Jedi Knights escape a hostile blockade to find allies and come across a young boy who may bring balance to the Force."
+    },
+    {
+      title: "Return of the Jedi",
+      releaseDate: "1983",
+      director: "Richard Marquand",
+      rating: 8.3,
+      description: "After a daring mission to rescue Han Solo from Jabba the Hutt, the Rebels dispatch to Endor to destroy a more powerful Death Star."
+    },
+    {
+      title: "Attack of the Clones",
+      releaseDate: "2002",
+      director: "George Lucas",
+      rating: 6.5,
+      description: "Ten years after initially meeting, Anakin Skywalker shares a forbidden romance with Padmé Amidala while his master Obi-Wan Kenobi investigates an assassination attempt."
+    },
+    {
+      title: "The Empire Strikes Back",
+      releaseDate: "1980",
+      director: "Irvin Kershner",
+      rating: 8.7,
+      description: "After the Rebels are brutally overpowered by the Empire, Luke Skywalker begins Jedi training with Yoda while his friends are pursued by Darth Vader."
+    },
+    {
+      title: "The Force Awakens",
+      releaseDate: "2015",
+      director: "J.J. Abrams",
+      rating: 7.8,
+      description: "As a new threat to the galaxy rises, Rey, a desert scavenger, and Finn, an ex-stormtrooper, join Han Solo and Chewbacca to search for the one hope of restoring peace."
+    },
+    {
+      title: "The Last Jedi",
+      releaseDate: "2017",
+      director: "Rian Johnson",
+      rating: 7.0,
+      description: "Rey develops her newly discovered abilities with the guidance of Luke Skywalker, who is unsettled by the strength of her powers."
+    },
+    {
+      title: "The Rise of Skywalker",
+      releaseDate: "2019",
+      director: "J.J. Abrams",
+      rating: 6.6,
+      description: "The surviving members of the resistance face the First Order once again, and the legendary conflict between the Jedi and the Sith reaches its peak."
+    },
+    {
+      title: "Solo: A Star Wars Story",
+      releaseDate: "2018",
+      director: "Ron Howard",
+      rating: 6.9,
+      description: "During an adventure into the criminal underworld, Han Solo meets his future co-pilot Chewbacca and encounters Lando Calrissian years before joining the Rebellion."
+    }
+  ];
+  
