dab63e755fe6aa81eaab2fc30383bd1ca96be463
Krutivas Pradhan <72146433+krutivaspradhan@users.noreply.github.com>
2024-07-30 12:04:11 -0500

    Update starwarScript.js

--- scripts/starwarScript.js
+++ scripts/starwarScript.js
@@ -1,38 +1,73 @@
-$(document).ready(function(){
+$(document).ready(function() {
     console.log("ready!");
 
-    // Initialize an array of additional Star Wars movies
-    let additionalMovies = [
-        "The Empire Strikes Back",
-        "The Force Awakens",
-        "The Last Jedi",
-        "The Rise of Skywalker",
-        "Solo: A Star Wars Story"
-    ];
+    // Load movie data from data.js
+    const movies = moviesData;
+    let additionalMovies = [...movies];
 
-    // Shuffle the array
-    for (let i = additionalMovies.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [additionalMovies[i], additionalMovies[j]] = [additionalMovies[j], additionalMovies[i]];
+    // Function to display movie details
+    function displayMovieDetails(movie) {
+        return `
+            <div>
+                <h4>${movie.title}</h4>
+                <p>Release Date: ${movie.releaseDate}</p>
+                <p>Director: ${movie.director}</p>
+                <p>Rating: ${movie.rating}</p>
+                <p>Description: ${movie.description}</p>
+                <div class="comments">
+                    <h5>Comments</h5>
+                    <ul class="comments-list"></ul>
+                    <textarea class="comment-input" placeholder="Add a comment"></textarea>
+                    <button class="btn btn-primary add-comment">Add Comment</button>
+                </div>
+            </div>
+        `;
     }
 
-    // Create a click event for the button
-    $("#addMovie").on("click", function() {
-        // Check if there are any movies left to add
-        if (additionalMovies.length > 0) {
-            // Get the first movie from the array
-            const movie = additionalMovies[0];
+    // Add movies to the list on page load
+    additionalMovies.forEach((movie, index) => {
+        $("#movieList").append(`
+            <li class="list-group-item" data-title="${movie.title}">
+                ${movie.title} 
+                <button class="btn btn-link show-details">More Info</button>
+                <div class="movie-details" style="display: none;">
+                    ${displayMovieDetails(movie)}
+                </div>
+            </li>
+        `);
+    });
 
-            // Add the movie to the DOM
-            $("#movieList").append(`<li class="list-group-item">${movie}</li>`);
+    // Show details when clicking on "More Info"
+    $(document).on("click", ".show-details", function() {
+        $(this).siblings(".movie-details").toggle();
+    });
 
-            // Remove the first movie from the array
-            additionalMovies.shift();
+    // Show all details
+    $("#showAll").on("click", function() {
+        $(".movie-details").show();
+    });
 
-            // If we've reached the end of the array, disable the button
-            if (additionalMovies.length === 0) {
-                $(this).prop("disabled", true).text("No More Movies");
-            }
+    // Hide all details
+    $("#hideAll").on("click", function() {
+        $(".movie-details").hide();
+    });
+
+    // Recommend a random movie
+    $("#recommendMovie").on("click", function() {
+        const randomIndex = Math.floor(Math.random() * additionalMovies.length);
+        const randomMovie = additionalMovies[randomIndex];
+        alert(`We recommend you to watch: ${randomMovie.title}`);
+    });
+
+    // Add comment functionality
+    $(document).on("click", ".add-comment", function() {
+        const commentInput = $(this).siblings(".comment-input");
+        const commentText = commentInput.val().trim();
+        
+        if (commentText) {
+            const commentList = $(this).siblings(".comments-list");
+            commentList.append(`<li>${commentText}</li>`);
+            commentInput.val(''); // Clear the input field
         }
     });
 });
